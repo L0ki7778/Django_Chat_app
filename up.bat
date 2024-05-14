b0d4353047b1cf061a15ee0da4e89b80c5b18cea
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-05-14 23:21:56 +0200

    changed

---
+++ up.bat
@@ -0,0 +1,11 @@
+@echo off
+
+if "%1"=="" (
+    echo Bitte geben Sie einen Commit-Nachricht ein.
+    exit /b
+)
+
+set commit_message=%1
+git add .
+git commit -m "%commit_message%"
+git push origin master
